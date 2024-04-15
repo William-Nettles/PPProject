599a7361bb8676f493910a3152d16c2b0db907da
William Nettles <willnettles@gmail.com>
2024-04-15 14:56:12 -0400

    commit for ppp

---
+++ src/test/java/com/revature/PPProject/PpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.PPProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
